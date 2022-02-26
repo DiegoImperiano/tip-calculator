fdcbece15177e9d17ee4407f095a95a474994d9f
Diego Imperiano <imperiano.cn@gmail.com>
2022-02-26 14:46:02 -0300

    organização dos arquivos e criação do READE.me

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
+custom.onkeyup = function () {
+  let bill = document.getElementById('bill').value
+  let numOfPeople = document.getElementById('people').value
+  let custom = document.querySelector('input#custom')
+  let num = custom.value
+  if (bill == '') {
+    document.getElementById('totaltip').innerHTML = '$0.00'
+    document.getElementById('tip').innerHTML = '$0.00'
+
+    document.getElementsByClassName('inform')[0].style.border = 'solid 2px red'
+  } else if (numOfPeople == '') {
+    document.getElementById('totaltip').innerHTML = '$0.00'
+    document.getElementById('tip').innerHTML = '$0.00'
+
+    document.getElementsByClassName('inform')[1].style.border = 'solid 2px red'
+  } else {
+    document.getElementsByClassName('inform')[0].style.border = 'none'
+    document.getElementsByClassName('inform')[1].style.border = 'none'
+    let total = bill * (num / 100)
+    let final = total + Number(bill)
+
+    document.getElementById('totaltip').innerHTML =
+      '$' + Number(final).toFixed(2)
+    let cada = total / numOfPeople
+    document.getElementById('tip').innerHTML = '$' + Number(cada).toFixed(2)
+    console.log(total, final, bill, num)
+  }
+}
 function calc(n) {
   let bill = document.getElementById('bill').value
   let numOfPeople = document.getElementById('people').value
   let num = n
-  if (num == 0) {
-    document.getElementById('tip').innerHTML = 'No Tip'
 
-    document.getElementById('totaltip').innerHTML = 'customer did not like'
+  if (bill == '') {
+    document.getElementById('totaltip').innerHTML = '$0.00'
+    document.getElementById('tip').innerHTML = '$0.00'
+
+    document.getElementsByClassName('inform')[0].style.border = 'solid 2px red'
+  } else if (numOfPeople == '') {
+    document.getElementById('totaltip').innerHTML = '$0.00'
+    document.getElementById('tip').innerHTML = '$0.00'
+
+    document.getElementsByClassName('inform')[1].style.border = 'solid 2px red'
   } else {
-    if (bill == '' || numOfPeople == '') {
-      document.getElementById('totaltip').innerHTML = '$0.00'
-      document.getElementById('tip').innerHTML = '$0.00'
+    document.getElementsByClassName('inform')[0].style.border = 'none'
+    document.getElementsByClassName('inform')[1].style.border = 'none'
+    let total = bill * num
+    let final = total + Number(bill)
 
-      document.getElementsByClassName('inform')[0].style.border = 'solid red'
-      document.getElementsByClassName('inform')[1].style.border = 'solid red'
-    } else {
-      let total = bill * num
-      let final = total + Number(bill)
-
-      document.getElementById('totaltip').innerHTML =
-        '$' + Number(final).toFixed(2)
-      let cada = total / numOfPeople
-      document.getElementById('tip').innerHTML = '$' + Number(cada).toFixed(2)
-    }
+    document.getElementById('totaltip').innerHTML =
+      '$' + Number(final).toFixed(2)
+    let cada = total / numOfPeople
+    document.getElementById('tip').innerHTML = '$' + Number(cada).toFixed(2)
   }
 }
 function reset() {
@@ -29,4 +58,5 @@ function reset() {
   document.getElementById('tip').innerHTML = '$0.00'
   document.getElementsByClassName('inform')[0].style.border = 'none'
   document.getElementsByClassName('inform')[1].style.border = 'none'
+  document.querySelector('input#custom').innerHTML = ''
 }
